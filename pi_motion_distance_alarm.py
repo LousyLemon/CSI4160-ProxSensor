b9c7e0daacf895732d9826ca170d97f4727225ac
ZacharyMccallum <z19mccallum@gmail.com>
2022-12-02 09:33:39 -0500

    Update pi_motion_distance_alarm.py
    added publish logic, added sql query logic

--- pi_motion_distance_alarm.py
+++ pi_motion_distance_alarm.py
@@ -1,9 +1,12 @@
 # pi_motion_distance_alarm.py
 import RPi.GPIO as GPIO
-import time
-import mysql.connector
+import time, datetime, os, sqlalchemy, pymysql
+# Import GCP requirements
+from google.cloud import pubsub_v1
+from google.cloud.sql.connector import Connector
 
-# Declare GPIO Pins
+
+# Declare Raspberry Pi GPIO Pins
 PIR = 4 #Physical pin: 7
 TRIG = 17 #Physical pin: 11
 ECHO = 27 #Physical pin: 13
@@ -19,30 +22,87 @@ GPIO.setup(PIR, GPIO.IN)
 # Set initial value to false (Not measuring anything)
 GPIO.output(TRIG, False)
 
-def triggerAlarm(type, dist, time):
+
+# initialize connector object
+connector = Connector()
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'csi4160-pisub.json'
+
+publisher = pubsub_v1.PublisherClient()
+topic_path = 'projects/cheon-csi4160-f22/topics/pi-events'
+
+
+#Establish connection object construction
+def getconn() -> pymysql.connections.Connection:
+	with Connector() as connector:
+		conn: pymysql.connections.Connection = connector.connect(
+			"cheon-csi4160-f22:us-central1:cheon-4160",
+			"pymysql",
+			user="root",
+			password="bowers",
+			database="DB_ALARMS",
+		)
+	return conn
+
+
+# Create connection pool
+pool = sqlalchemy.create_engine(
+	"mysql+pymysql://",
+	creator=getconn,
+)
+
+def triggerAlarm(type, dist, time_to_trigger):
+    # Function Objectives:
+    # 1 - Determine Alarm type
+    # 2 - Prepare queries according to [1]
+    # 3 - Prepare message for Pub/Sub according to [1]
+    # 4 - Send message to Pub/Sub topic
+    # 5 - Connect to DB, execute query, return full table contents
+    # 5 - 
     # send mesage to pub/sub
-    #import publisher.py script and run here? or copy contents?
-    # Contents:
-        # time, type(of trigger), reporting device, dist
     
-    # send query to database
-    # Create connection object
-    mydb = mysql.connector.connect(
-        host="PUBLIC-IP-HERE", 
-        user="root",
-        password="PASSWORD-HERE",
-        database="DB_ALARMS"
-    )
-    mycursor = mydb.cursor()
-    time_now = 'time' #ntp time
+    #mycursor = mydb.cursor()
+    topic_path = 'projects/cheon-csi4160-f22/tpoics/pi-events'
+    timestamp = datetime.now()
     if type == 'Time-evoked':
-        query = "INSERT INTO time_alarms (time_now, min_distance) VALUES (%s, %s)"
-        val = (time_now, dist)
-        mycursor.execute(query, val)
+        # Prepare time alarm query
+        insert_stmt = sqlalchemy.text(
+    	    "INSERT INTO time_alarms (time_now, min_distance) VALUES (:timestamp, :min_distance)"
+	    )
+
+        # Create output message to push to pub/sub topic
+        data = f"{timestamp} - {type} Alarm Detected - Minimum Distance: {dist}"
+        data = data.encode('utf-8')
+        print("Sending details to Pub/Sub")
+        future = publisher.publish(topic_path, data)
+        print(f'Published message id {future.result()}')
+
+        # Connect to instance, insert collected data
+        with pool.connect() as db_conn:
+            db_conn.execute(insert_stmt, time_now=timestamp, min_distance="230")
+            result = db_conn.execute("SELECT * FROM prox_alarms").fetchall()
+	    # result = db_conn.execute("SELECT * FROM time_alarms").fetchall()
+            for row in result:
+	            print(row)
+
     elif type == 'Proximity':
-        query = "INSERT INTO prox_alarms (time_now, time_to_trigger) VALUES (%s, %s)"
-        val = (time_now, dist)
-        mycursor.execute(query, val)
+        # prepare proximity alarm query
+        insert_stmt =  sqlalchemy.text(
+            "INSERT INTO prox_alarms (time_now, time_to_trigger) VALUES (:timestamp, :time_to_trigger)"
+        )
+        # Create output message to push to pub/sub topic
+        data = f"{timestamp} - {type} Alarm Detected - Time to trigger: {time_to_trigger}"
+        data = data.encode('utf-8')
+        print("Sending details to Pub/Sub")
+        future = publisher.publish(topic_path, data)
+        print(f'Published message id {future.result()}')
+    
+        with pool.connect() as db_conn:
+            db_conn.execute(insert_stmt, time_now=timestamp, time_to_trigger="10s")
+            db_conn.execute("SELECT * FROM time_alarms").fetchall()
+            # result = db_conn.execute("SELECT * FROM time_alarms").fetchall()
+            for row in result:
+	            print(row)
 
 # Logic to support Ultrasonic sensor
 
@@ -74,7 +134,6 @@ while True:
             pulse_duration = pulse_end - pulse_start
             distance = pulse_duration * 17150
             distance = round(distance+1.15, 2)
-  
             if distance<=RANGE_THRESHOLD and TIME_TRHRESHOLD<=time_counter:
                 print("distance:",distance,"cm")
                 triggerAlarm('Time-evoked', distance)
