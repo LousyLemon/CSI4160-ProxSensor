ba932e9ee7a66b0acb29e916e7c4a81edb5ecabe
ZacharyMccallum <z19mccallum@gmail.com>
2022-12-02 09:59:07 -0500

    Update pi_motion_distance_alarm.py
    cleaned up code and added time-tracking elements

--- pi_motion_distance_alarm.py
+++ pi_motion_distance_alarm.py
@@ -11,7 +11,7 @@ PIR = 4 #Physical pin: 7
 TRIG = 17 #Physical pin: 11
 ECHO = 27 #Physical pin: 13
 TIME_TRHRESHOLD = 10 # Measured in seconds?
-RANGE_THRESHOLD = 375 # Measured in centimeters
+OUTER_RANGE_THRESHOLD = 375 # Measured in centimeters
 RANGE_TRIGGER = 50 # Measured in centimeters
 
 # Set up I/O declarations
@@ -52,102 +52,117 @@ pool = sqlalchemy.create_engine(
 )
 
 def triggerAlarm(type, dist, time_to_trigger):
-    # Function Objectives:
-    # 1 - Determine Alarm type
-    # 2 - Prepare queries according to [1]
-    # 3 - Prepare message for Pub/Sub according to [1]
-    # 4 - Send message to Pub/Sub topic
-    # 5 - Connect to DB, execute query, return full table contents
-    # 5 -
-    # send mesage to pub/sub
+	# Function Objectives:
+	# 1 - Determine Alarm type
+	# 2 - Prepare queries according to [1]
+	# 3 - Prepare message for Pub/Sub according to [1]
+	# 4 - Send message to Pub/Sub topic
+	# 5 - Connect to DB, execute query, return full table contents
 
-    #mycursor = mydb.cursor()
-    topic_path = 'projects/cheon-csi4160-f22/tpoics/pi-events'
-    timestamp = datetime.now()
-    if type == 'Time-evoked':
-        # Prepare time alarm query
-        insert_stmt = sqlalchemy.text(
-    	    "INSERT INTO time_alarms (time_now, min_distance) VALUES (:timestamp, :min_distance)"
-	    )
+	#mycursor = mydb.cursor()
+	topic_path = 'projects/cheon-csi4160-f22/tpoics/pi-events'
+	timestamp = datetime.now()
 
-        # Create output message to push to pub/sub topic
-        data = f"{timestamp} - {type} Alarm Detected - Minimum Distance: {dist}"
-        data = data.encode('utf-8')
-        print("Sending details to Pub/Sub")
-        future = publisher.publish(topic_path, data)
-        print(f'Published message id {future.result()}')
+	# [1] Determine Alarm Type
+	if type == 'Time-evoked':
+		# [2] Prepare time alarm query
+		insert_stmt = sqlalchemy.text(
+			"INSERT INTO time_alarms (time_now, min_distance) VALUES (:timestamp, :min_distance)"
+		)
 
-        # Connect to instance, insert collected data
-        with pool.connect() as db_conn:
-            db_conn.execute(insert_stmt, time_now=timestamp, min_distance="230")
-            result = db_conn.execute("SELECT * FROM prox_alarms").fetchall()
-	    # result = db_conn.execute("SELECT * FROM time_alarms").fetchall()
-            for row in result:
-	            print(row)
+		# [3] Create output message to push to pub/sub topic
+		data = f"{timestamp} - {type} Alarm Detected - Minimum Distance: {dist}"
+		data = data.encode('utf-8')
+		
+		# [4] Send message and show message id
+		print("Sending details to Pub/Sub")
+		future = publisher.publish(topic_path, data)
+		print(f'Published message ID: {future.result()}')
 
-    elif type == 'Proximity':
-        # prepare proximity alarm query
-        insert_stmt =  sqlalchemy.text(
-            "INSERT INTO prox_alarms (time_now, time_to_trigger) VALUES (:timestamp, :time_to_trigger)"
-        )
-        # Create output message to push to pub/sub topic
-        data = f"{timestamp} - {type} Alarm Detected - Time to trigger: {time_to_trigger}"
-        data = data.encode('utf-8')
-        print("Sending details to Pub/Sub")
-        future = publisher.publish(topic_path, data)
-        print(f'Published message id {future.result()}')
+		# [5] Connect to instance, insert collected data
+		with pool.connect() as db_conn:
+			db_conn.execute(insert_stmt, time_now=timestamp, min_distance="230")
+			result = db_conn.execute("SELECT * FROM prox_alarms").fetchall()
+		# result = db_conn.execute("SELECT * FROM time_alarms").fetchall()
+			for row in result:
+				print(row)
 
-        with pool.connect() as db_conn:
-            db_conn.execute(insert_stmt, time_now=timestamp, time_to_trigger="10s")
-            db_conn.execute("SELECT * FROM time_alarms").fetchall()
-            # result = db_conn.execute("SELECT * FROM time_alarms").fetchall()
-            for row in result:
-	            print(row)
+	elif type == 'Proximity':
+		# [2] prepare proximity alarm query
+		insert_stmt =  sqlalchemy.text(
+			"INSERT INTO prox_alarms (time_now, time_to_trigger) VALUES (:timestamp, :time_to_trigger)"
+		)
+		# [3] Create output message to push to pub/sub topic
+		data = f"{timestamp} - {type} Alarm Detected - Time to trigger: {time_to_trigger}"
+		data = data.encode('utf-8')
+		
+		# [4] Send message and show message id
+		print("Sending details to Pub/Sub")
+		future = publisher.publish(topic_path, data)
+		print(f'Published message ID: {future.result()}')
+
+		# [5] Connect to instance, insert collected data
+		with pool.connect() as db_conn:
+			db_conn.execute(insert_stmt, time_now=timestamp, time_to_trigger="10s")
+			db_conn.execute("SELECT * FROM time_alarms").fetchall()
+			# result = db_conn.execute("SELECT * FROM time_alarms").fetchall()
+			for row in result:
+				print(row)
 
 # Logic to support Ultrasonic sensor
 
 while True:
-    # Wait for the motion detector to activate before attempting to find an object
-    # pir.wait_for_motion()
-    # Action taken:
-    # contents of reference for ultrasonic sensor
-    # do this until target is out of range
+	# Wait for the motion detector to activate before attempting to find an object
+	# pir.wait_for_motion()
+	# Action taken:
+	# contents of reference for ultrasonic sensor
+	# do this until target is out of range
 
-    # if the range < 200cm then we want to start a counter
-    # count until time THRESHOLD
-    # if threshold is met / hit, then we triggerAlarm()
-    # Create db entry -> prepared statement
-    # Send message to pub/sub topic
-    # if the range < 50 cm then we want to trigger the alarm right away
-    # Range threshold
-    try:
-        while True:
-            GPIO.output(TRIG, True)
-            time.sleep(0.00001)
-            GPIO.output(TRIG, False)
+	# if the range < 200cm then we want to start a counter
+	# count until time THRESHOLD
+	# if threshold is met / hit, then we triggerAlarm()
+	# Create db entry -> prepared statement
+	# Send message to pub/sub topic
+	# if the range < 50 cm then we want to trigger the alarm right away
+	# Range threshold
+	pir_signal=GPIO.input(PIR)
+	start_time = time.time()
+	try:
+		while pir_signal==1: # condition should be - while PIR == HIGH
+			print("Motion detected - Measuring")
+			# When the signal is detecting something in range and moving
+			GPIO.output(TRIG, True)
+			time.sleep(0.00001)
+			GPIO.output(TRIG, False)
+			
+			# Math to handle trig-echo time related-distance calculations
+			while GPIO.input(ECHO)==0:
+				pulse_start = time.time()
+			while GPIO.input(ECHO)==1:
+				pulse_end = time.time()
+			pulse_duration = pulse_end - pulse_start
+			distance = pulse_duration * 17150
+			distance = round(distance+1.15, 2)
+			print(f"Distance: {distance}")
+			#Track length (in time) the loop has executed for
+			end_time = time.time()
+			time_counter= end_time - start_time
+			print(f"Time counter: {time_counter}")
 
-            # Math to handle trig-echo time related-distance calculations
-            while GPIO.input(ECHO)==0:
-                pulse_start = time.time()
-            while GPIO.input(ECHO)==1:
-                pulse_end = time.time()
-            pulse_duration = pulse_end - pulse_start
-            distance = pulse_duration * 17150
-            distance = round(distance+1.15, 2)
-            if distance<=RANGE_THRESHOLD and TIME_TRHRESHOLD<=time_counter:
-                print("distance:",distance,"cm")
-                triggerAlarm('Time-evoked', distance)
-                i=1
+			if distance<=OUTER_RANGE_THRESHOLD and TIME_TRHRESHOLD<=time_counter:
+				print("Distance:",distance,"cm")
+				triggerAlarm('Time-evoked', distance)
+				i=1
 
-            if distance<=RANGE_TRIGGER:
-                triggerAlarm('Proximity', distance)
-                i=0
-                time.sleep(2)
+			if distance<=RANGE_TRIGGER:
+				triggerAlarm('Proximity', distance)
+				i=0
+				time.sleep(2)
 
-    except KeyboardInterrupt:
-        GPIO.cleanup()
+	except KeyboardInterrupt:
+		GPIO.cleanup()
 
-    print("Motion detected - LED Active")
-    pir.wait_for_no_motion()
-    red_led.off()
-    print("Motion Stopped")
+	
+	pir.wait_for_no_motion()
+	red_led.off()
+	print("Motion Stopped")
